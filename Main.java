7e4d364efeb6bd247b4ab5813a3c491c20adf29f
nathan <wallaby5716@yahoo.com>
2019-12-30 21:30:25 -0700

    yeet

---
+++ Main.java
@@ -0,0 +1,9 @@
+package ChessAyeEye;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hewwo Jowo! " + System.getProperty("java.version"));
+    }
+    
+}
