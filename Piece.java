a23859ed16d7a8a4a68393e9c50ee98d7429a08e
Joe Winfield <jwinfield277@gmail.com>
2020-01-08 19:36:31 -0600

    move list now assigned on piece creation
    :)

--- Piece.java
+++ Piece.java
@@ -11,40 +11,40 @@ TODO(?):
 */
 
 public class Piece {
-
+    int[] moves;
     enum Type{
       PAWN, ROOK, KNIGHT, BISHOP_WHITE, BISHOP_BLACK, KING, QUEEN, NONE;
 
     // '0' is to split moves and attacks for pawns
 
-    static int[] PAWN_MOVES = {16,32,0,15,17};
+    private static int[] PAWN_MOVES = {16,32,0,15,17};
 
-    static int[] ROOK_MOVES = {
+    private static int[] ROOK_MOVES = {
         1,2,3,4,5,6,7,              //left right
         16,32,48,64,80,96,112};     //up down
 
 
-    static int[] KNIGHT_MOVES = {14,31,33,18};
+    private static int[] KNIGHT_MOVES = {14,31,33,18};
 
-    static int[] BISHOP_WHITE_MOVES = {
+    private static int[] BISHOP_WHITE_MOVES = {
         17,34,51,68,85,102,119,     //top left to bottom right
         15,30,45,60,75,90};         //inner top right to upper bottom left
 
-    static int[] BISHOP_BLACK_MOVES = {
+    private static int[] BISHOP_BLACK_MOVES = {
         15,30,45,60,75,90,105,      //top right to botton left
         17,34,51,68,85,102};        //inner top left to upper bottom right
 
-    static int[] QUEEN_MOVES = {
+    private static int[] QUEEN_MOVES = {
         1,2,3,4,5,6,7,              //left right
         16,32,48,64,80,96,112,      //up down
         15,30,45,60,75,90,105,      //black long diagonal
         17,34,51,68,85,102,119};    //white long diagonal
 
     //the move 2 case is a special case for something i dont understand
-    static int[] KING_MOVES = {1,2,15,16,17};
+    private static int[] KING_MOVES = {1,2,15,16,17};
     }
 
-    public int[] getMoveArray(Type type){
+    private int[] getMoveArray(Type type){
       switch(type){
         case PAWN:
           return Type.PAWN_MOVES;
@@ -75,18 +75,22 @@ public class Piece {
         this.color = color;
         this.name = piece;
         this.type = Type.valueOf(piece);
+        this.moves = getMoveArray(this.type);
+
     }
 
     public Piece(String name, String type, String color) {
         this.color = color;
         this.name = name;
         this.type = Type.valueOf(type);
+        this.moves = getMoveArray(this.type);
     }
 
     public Piece() {
         this.color = "NONE";
         this.name = "NONE";
         this.type = Type.valueOf("NONE");
+        this.moves = getMoveArray(this.type);
     }
 
 
